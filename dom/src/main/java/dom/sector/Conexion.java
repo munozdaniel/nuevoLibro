832c2687a19d5c635f7f12c7a06ea1fbfb6a43ab
munozdaniel <cipoleto@gmail.com>
2015-08-10 13:18:25 -0300

    Agregado Conexion

---
+++ dom/src/main/java/dom/sector/Conexion.java
@@ -0,0 +1,40 @@
+package dom.sector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class Conexion {
+	@SuppressWarnings("finally")
+	public static Connection GetConnectionGestionUsuarios() {
+		Connection conexion = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String servidor = "jdbc:mysql://192.168.42.14/gestionusuarios";
+			String usuarioDB = "root";
+			String passwordDB = "infoimps";
+			conexion = DriverManager.getConnection(servidor, usuarioDB,
+					passwordDB);
+		} catch (ClassNotFoundException ex) {
+			JOptionPane.showMessageDialog(null, ex,
+					"Error1 en la Conexión con la BD " + ex.getMessage(),
+					JOptionPane.ERROR_MESSAGE);
+			conexion = null;
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, ex,
+					"Error2 en la Conexión con la BD " + ex.getMessage(),
+					JOptionPane.ERROR_MESSAGE);
+			conexion = null;
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, ex,
+					"Error3 en la Conexión con la BD " + ex.getMessage(),
+					JOptionPane.ERROR_MESSAGE);
+			conexion = null;
+		} finally {
+			return conexion;
+		}
+	}
+}
